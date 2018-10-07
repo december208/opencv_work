79f73d716c8556fe5898add851d2305ce6768ad2
zer0 <endlosaugust@gmail.com>
2018-10-07 18:57:58 +0800

    be able to set the number of obj

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,67 +1,93 @@
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
 using namespace std;
 
-Mat src, src_gray, edge, blured;
-vector<vector<Point> > contours;
+
+//fuction prototype
+static void edge_CB(int, void*);
+static void rect_CB(int, void*);
+bool compareArcLength(vector<Point> a, vector<Point> b);
+
+
+//global parameters
+Mat src, src_gray, blured, edge, dst;
 int low_thres=50;
 int up_thres=100;
 int MCitera = 3;
+int n_obj = 3;
 static int MAX_ITERATE = 20;
 static int MAX_THRES = 400;
+static int MAX_N_obj = 10;
 char win_name[] = "Display Image";
 
 
-static void track_CB(int, void*);
-
-
 int main(int argc, char** argv ){
+
     if ( argc != 2 ){
         printf("usage: DisplayImage.out <Image_Path>\n");
         return -1;
     }
-    src = imread( argv[1], 1 );
+    src = imread( argv[1], 1);
     if ( !src.data ){
         printf("No image data \n");
         return -1;
     }
 
-
     namedWindow(win_name, WINDOW_KEEPRATIO);
-    createTrackbar("UP_Threshold", win_name, &up_thres, MAX_THRES, track_CB);
-    createTrackbar("LOW_Threshold", win_name, &low_thres, MAX_THRES, track_CB);
-    createTrackbar("MORPH_CLOSE_ITERATE", win_name, &MCitera, MAX_ITERATE, track_CB);
+    createTrackbar("UP Threshold: ", win_name, &up_thres, MAX_THRES, edge_CB);
+    createTrackbar("LOW Threshold: ", win_name, &low_thres, MAX_THRES, edge_CB);
+    createTrackbar("MORPH CLOSE ITERATE: ", win_name, &MCitera, MAX_ITERATE, edge_CB);
+    createTrackbar("Number of object: ", win_name, &n_obj, MAX_N_obj, rect_CB);
 
 
     
     cvtColor(src,src_gray,COLOR_BGR2GRAY);
-    //blur(src_gray, blured, Size(11,11), Point(-1,-1));
-    GaussianBlur(src_gray,blured,Size(7,7),0,0);
-    Canny(blured, edge,low_thres,up_thres,3);
-    morphologyEx(edge, edge, MORPH_CLOSE, Mat(), Point(-1,-1), MCitera);
-    imshow(win_name, edge);
+    GaussianBlur(src_gray,blured,Size(17,17),0,0);
+    edge_CB(0,0);
     waitKey(0);
-
-    vector<Vec4i> hierarchy;
-    findContours(edge, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-    
-    cout<<contours.size()<<endl;
-    
-    for(int i=0; i<contours.size(); i++){
-        drawContours(src, contours, i, Scalar(0,0,255), 5,8, hierarchy);
-    }
-    imshow(win_name, src);
+    rect_CB(0,0);
     waitKey(0);
     return 0;
 }
 
-static void track_CB(int, void*){
+static void edge_CB(int, void*){
     Canny(blured, edge, low_thres, up_thres, 3);
     morphologyEx(edge, edge, MORPH_CLOSE, Mat(), Point(-1,-1), MCitera);
     imshow(win_name, edge);
+}
+
+static void rect_CB(int, void*){
+    dst = src.clone();
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+    findContours(edge, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+    vector<vector<Point> > contours_poly( contours.size() );
+    
+    for(int i=0; i<contours.size(); i++){
+        approxPolyDP(Mat(contours[i]), contours_poly[i], 50, true);
+        //cout<<contours_poly[i].size()<<endl;
+        //drawContours(src, contours_poly, i, Scalar(0,0,255), 5,8);
+    }
+    sort(contours_poly.begin(), contours_poly.end(), compareArcLength);
+    vector<RotatedRect> minRects(n_obj);
+    for(int i=0; i<n_obj; i++){
+        minRects[i] = minAreaRect(Mat(contours_poly[i]));
+        Point2f rect_points[4];
+        minRects[i].points( rect_points );
+        for( int j = 0; j < 4; j++ ){
+            line( dst, rect_points[j], rect_points[(j+1)%4], Scalar(0,0,255), 10, 8 );
+        }
+    }   
+    cout<<"sadasdsdasdsds\n";
+    imshow(win_name, dst);
+}
+
+bool compareArcLength(vector<Point> a, vector<Point> b){
+    return arcLength(a, true) > arcLength(b, true);
 }
 
 
