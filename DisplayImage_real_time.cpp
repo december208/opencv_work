99f3e4e01951d832c4be22d04f4fe0012ac41dce
zer0 <endlosaugust@gmail.com>
2018-10-12 18:29:03 +0800

    separate real-time fuctionality

--- DisplayImage_real_time.cpp
+++ DisplayImage_real_time.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <vector>
 #include <algorithm>
@@ -7,15 +6,14 @@
 using namespace cv;
 using namespace std;
 
-
 //fuction prototype
 static void edge_CB(int, void*);
 static void rect_CB(int, void*);
 bool compareArcLength(vector<Point> a, vector<Point> b);
-
+Mat preProcessImg(Mat input);
+Mat drawFindedObj(Mat src, Mat input);
 
 //global parameters
-Mat src, src_gray, blured, edge, dst;
 int low_thres=50;
 int up_thres=100;
 int MCitera = 3;
@@ -24,66 +22,77 @@ static int MAX_ITERATE = 20;
 static int MAX_THRES = 400;
 static int MAX_N_obj = 10;
 char win_name[] = "Display Image";
-
+VideoCapture cap(1);
 
 int main(int argc, char** argv ){
-
-    if ( argc != 2 ){
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    src = imread( argv[1], 1);
-    if ( !src.data ){
-        printf("No image data \n");
-        return -1;
-    }
 
     namedWindow(win_name, WINDOW_KEEPRATIO);
     createTrackbar("UP Threshold: ", win_name, &up_thres, MAX_THRES, edge_CB);
     createTrackbar("LOW Threshold: ", win_name, &low_thres, MAX_THRES, edge_CB);
     createTrackbar("MORPH CLOSE ITERATE: ", win_name, &MCitera, MAX_ITERATE, edge_CB);
     createTrackbar("Number of object: ", win_name, &n_obj, MAX_N_obj, rect_CB);
-
-
     
-    cvtColor(src,src_gray,COLOR_BGR2GRAY);
-    GaussianBlur(src_gray,blured,Size(17,17),0,0);
-    edge_CB(0,0);
-    waitKey(0);
-    rect_CB(0,0);
-    waitKey(0);
+    while(1){
+        if(!cap.isOpened()) cout<<"not yet\n";
+        else break;
+    }
+    Mat src, edge, dst;
+    while(cap.isOpened()){
+        cap>>src;
+        for(int i=0; i<5; i++) cap.grab();
+        edge = preProcessImg(src);
+        imshow(win_name, edge);
+        if(waitKey(10)>=0) break;;
+    }
+    while(cap.isOpened()){
+        cap>>src;
+        for(int i=0; i<5; i++) cap.grab();
+        edge = preProcessImg(src);
+        dst = drawFindedObj(src, edge);
+        imshow(win_name, dst);
+        if(waitKey(10)>=0) break;;
+    }   
     return 0;
 }
 
-static void edge_CB(int, void*){
-    Canny(blured, edge, low_thres, up_thres, 3);
-    morphologyEx(edge, edge, MORPH_CLOSE, Mat(), Point(-1,-1), MCitera);
-    imshow(win_name, edge);
+static void edge_CB(int, void*){}
+static void rect_CB(int, void*){}
+
+bool compareArcLength(vector<Point> a, vector<Point> b){
+    return contourArea(a, true) < contourArea(b, true);
 }
 
-static void rect_CB(int, void*){
-    dst = src.clone();
+Mat  preProcessImg(Mat input){
+    Mat src_gray, blured, edge, dst;
+    cvtColor(input,src_gray,COLOR_BGR2GRAY);
+    //GaussianBlur(src_gray,blured,Size(3,3),0,0);
+    Canny(src_gray, edge, low_thres, up_thres, 3);
+    morphologyEx(edge, edge, MORPH_CLOSE, Mat(), Point(-1,-1), MCitera);
+    return edge;
+}
+
+Mat drawFindedObj(Mat src, Mat input){
+    Mat output = src.clone();
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
-    findContours(edge, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+    findContours(input, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
     vector<vector<Point> > contours_poly( contours.size() );
-    
     for(int i=0; i<contours.size(); i++){
-        approxPolyDP(Mat(contours[i]), contours_poly[i], 50, true);
+        approxPolyDP(Mat(contours[i]), contours_poly[i], 100, true);
     }
     sort(contours_poly.begin(), contours_poly.end(), compareArcLength);
     vector<RotatedRect> minRects(n_obj);
+    if(n_obj>contours_poly.size()) return src;
     for(int i=0; i<n_obj; i++){
         minRects[i] = minAreaRect(Mat(contours_poly[i]));
         Point2f rect_points[4];
         minRects[i].points( rect_points );
         for( int j = 0; j < 4; j++ ){
-            line( dst, rect_points[j], rect_points[(j+1)%4], Scalar(0,0,255), 7, 8 );
+            line( output, rect_points[j], rect_points[(j+1)%4], Scalar(0,0,255), 2, 8 );
         }
-    }   
-    imshow(win_name, dst);
+    }
+    return output;
 }
 
-bool compareArcLength(vector<Point> a, vector<Point> b){
-    return arcLength(a, true) > arcLength(b, true);
-}
+
+
