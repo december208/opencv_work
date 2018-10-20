9927e939224adf0671a373cbcc794517a90f4bfd
zer0 <endlosaugust@gmail.com>
2018-10-21 00:17:31 +0800

    modify classify function

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -15,7 +15,7 @@ bool compareArea(vector<Point> a, vector<Point> b);
 Mat drawInfo(Mat src, vector<RotatedRect> minRects);
 Mat preProcess(Mat input);
 vector<RotatedRect> findObj( Mat input);
-vector<string> classifyObj(vector<RotatedRect> minRects);
+string classifyObj(RotatedRect minRect);
 
 int low_thres=100;
 int up_thres=200;
@@ -39,7 +39,13 @@ int main(int argc, char** argv ){
     color_CB(0,0);
     edge_CB(0,0);
     rect_CB(0,0);
-    while(1){int pressedKey = waitKey(0);if( pressedKey == 113) break;} //press q key to quit
+    while(1){
+        int pressedKey = waitKey(0);
+        if( pressedKey == 113) break;//press q key to quit
+        if( pressedKey == 'e') edge_CB(0,0);//press e key to show edge
+        if( pressedKey == 'r') rect_CB(0,0);//press r key to show bounding rectangle
+        if( pressedKey == 'w') color_CB(0,0);//press w key to show color mask
+    } 
     return 0;
 }
 
@@ -77,7 +83,7 @@ Mat  preProcess(Mat input){
     Mat edge;
     Canny(srcG+srcR, edge, low_thres, up_thres, 3);
     dilate(edge, edge, Mat(),Point(-1,-1), dilate_itera);
-    erode(edge, edge, Mat(),Point(-1,-1), dilate_itera);
+    //erode(edge, edge, Mat(),Point(-1,-1), dilate_itera);
     return edge;
 }
 
@@ -102,63 +108,53 @@ Mat drawInfo(Mat src, vector<RotatedRect> minRects){
     if(minRects.size()<n_obj) return src;
     Mat output = src.clone();
     Point2f shift(30,-30);
-    vector<string> result = classifyObj(minRects);
     for(int i=0; i<minRects.size(); i++){
         Point2f rect_points[4];
         minRects[i].points( rect_points );
         for( int j = 0; j < 4; j++ ){
             line( output, rect_points[j], rect_points[(j+1)%4], Scalar(0,0,0), 10, 8 );
         }
-        int center_x = minRects[i].center.x;
-        int center_y = minRects[i].center.y;
-        string coord = format("( %i, %i)", center_x, center_y);
+        string objType = classifyObj(minRects[i]);
+        //int center_x = minRects[i].center.x;
+        //int center_y = minRects[i].center.y;
+        //string coord = format("( %i, %i)", center_x, center_y);
         circle(output, minRects[i].center, 20, Scalar(0,0,0), CV_FILLED,8);
-        putText(output, result[i], minRects[i].center+shift, 0, 4, Scalar(0,0,0), 5, 8);
+        putText(output, objType, minRects[i].center+shift, 0, 4, Scalar(0,0,0), 5, 8);
     }
     return output;
 }
 
-vector<string> classifyObj(vector<RotatedRect> minRects){
-    vector<string> result;
+string classifyObj(RotatedRect minRect){
     float error = (float)ratioErrTolerance/100.0;
-    for(int i=0; i<minRects.size(); i++){
-        float sideA = minRects[i].size.width;
-        float sideB = minRects[i].size.height;
-        float ratio=0;
-        Point center = minRects[i].center;
-        if(sideA>sideB || sideA == sideB) ratio = sideA/sideB;
-        else ratio = sideB/sideA;
-        if(fabs(ratio-1.0) < error){
-            if(srcR.at<uchar>(center.y,center.x) == 255){
-                result.push_back("Red Cubic");
-                continue;
-            } 
-            else if(srcG.at<uchar>(center.y,center.x) == 255){
-                result.push_back("Green Cubic");
-                continue;
-            }
-            else{
-                result.push_back("UNKNOWN");
-                continue;
-            }
+    float sideA = minRect.size.width;
+    float sideB = minRect.size.height;
+    float ratio=0;
+    Point center = minRect.center;
+    if(sideA>sideB || sideA == sideB) ratio = sideA/sideB;
+    else ratio = sideB/sideA;
+    if(fabs(ratio-1.0) < error){
+        if(srcR.at<uchar>(center.y,center.x) == 255){
+            return "Red Cubic";
+        } 
+        else if(srcG.at<uchar>(center.y,center.x) == 255){
+            return "Green Cubic";
         }
-        else if(fabs(ratio-2.0) < error){
-            if(srcR.at<uchar>(center.y,center.x) == 255){
-                result.push_back("UNKNOWN");
-                continue;
-            }
-            else if(srcG.at<uchar>(center.y,center.x) == 255){
-                result.push_back("Green Oval");
-                continue;
-            }
-            else{
-                result.push_back("UNKNOWN");
-                continue;
-            }
+        else{
+            return "UNKNOWN";
         }
-        else result.push_back("UNKNOWN");
     }
-    return result;
+    else if(fabs(ratio-2.0) < error){
+        if(srcR.at<uchar>(center.y,center.x) == 255){
+            return "UNKNOWN";
+        }
+        else if(srcG.at<uchar>(center.y,center.x) == 255){
+            return "Green Oval";
+        }
+        else{
+            return "UNKNOWN";
+        }
+    }
+    else return "UNKNOWN";
 }
 
 void setupWindow(){
